6c293df39570bb672d6c5a7f020c481773d70be6
Ghabryel <ghabryelhenrique@hotmail.com>
2022-07-04 06:31:15 -0300

    Ajuste error message

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask, Response, request
+from flask import Flask, Response, request, session, make_response
 import mysql.connector
-import json
 
 app = Flask(__name__)
+app.secret_key = "super secret key"
 con = mysql.connector.connect(
-    host='localhost', database='cadastro', user='root', password='xxxx')
+    host='localhost', database='cadastro', user='root', password='1234')
 
 if con.is_connected():
     db_ifo = con.get_server_info()
@@ -13,25 +13,47 @@ if con.is_connected():
     cursor = con.cursor(buffered=True)
     cursor.execute('select database()')
 
+
 @app.route('/register', methods=["POST"])
 def cria_usuario():
-    nome = request.body.get('usuario')
-    email = request.body.get('email@email.com')
-    cpf = request.body.get(123)
-    senha = request.body.get('123456')
+    nome = request.json.get('nome')
+    email = request.json.get('email')
+    cpf = request.json.get('cpf')
+    senha = request.json.get('senha')
 
     cursor = con.cursor(buffered=True)
 
-    # Insert 3 records
     names = (nome, email, cpf, senha)
-    stmt_insert = "INSERT INTO usuarios (nome, email, cpf, senha) VALUES (%s, %s, %s, %s)"
-    cursor.executemany(stmt_insert, (names,))
+    try:
+        stmt_insert = "INSERT INTO users (nome, email, cpf, senha) VALUES (%s, %s, %s, %s)"
+        cursor.executemany(stmt_insert, (names,))
 
-    # Make sure data is committed to the database
-    con.commit()
+        # Make sure data is committed to the database
+        con.commit()
 
-    cursor.close()
-    con.close()
-    return 'Sucesso'
+        cursor.close()
+        return 'Sucesso'
+
+    except:
+        return 'Informe todas as informações', 404
+
+
+@app.route('/login', methods=["GET","POST"])
+def login():
+    email = request.json.get('email')
+    senha = request.json.get('password')
+    cursor = con.cursor(buffered=True)
+    if request.method == 'POST':
+        try:
+            cursor.execute('SELECT * FROM users WHERE email=%s AND senha=%s', (email, senha))
+            record = cursor.fetchone()
+            print(record)
+            session['loggedin'] = True
+            session['username'] = record[1]
+            return make_response(email)
+
+        except:
+            return 'Email/Senha Incorreta, Tente Novamente', 404
+
 
 app.run()
